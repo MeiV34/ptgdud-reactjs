a90e91e07f9b16660f987c9ee5b47bed45df8ddf
MeiV34 <vongocthuyvy31704@gmail.com>
2025-02-19 20:28:10 +0700

    vy init

---
+++ src/exercise-1-functional/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = ({ contact }) => {
+  return (
+    <div>
+      <h2>{contact.name}</h2>
+      <p>Email: {contact.email}</p>
+      <p>Phone: {contact.phone}</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./exercise-1-functional/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
